f8bc4fa02031c04a2ef00720172d751cd3e01901
Sebastian Goscik <sebastian.goscik@live.co.uk>
2021-09-02 23:33:32 +0100

    Allow strings for XP

--- CardGenerator.py
+++ CardGenerator.py
@@ -734,9 +734,7 @@ class MonsterCardLayout(CardLayout):
         self.reactions = reactions
 
         self.challenge_rating = challenge_rating
-        if int(experience_points) < 0:
-            raise ValueError("Experience points must be positive")
-        self.experience_points = "{:,}".format(int(experience_points))
+        self.experience_points = experience_points
         self.source = source
 
     def _draw_back(self, canvas):
